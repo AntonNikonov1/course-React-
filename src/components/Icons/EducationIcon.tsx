8fe866bfeca4df696a7d1933802e2087ab19b7c8
AntonNikonov1 <125year125@gmail.com>
2024-05-18 11:22:40 +0200

    Add type condition in EducationIcon component.

--- src/components/Icons/EducationIcon.tsx
+++ src/components/Icons/EducationIcon.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
+import SchoolIcon from "@mui/icons-material/School";
 import { IconComponentProps } from "./Icon.interface";
 
 export const EducationIcon = ({ type, className }: IconComponentProps) => {
-  return <SchoolOutlinedIcon className={className} />;
+  if (type === "outline") {
+    return <SchoolOutlinedIcon className={className} />;
+  }
+  return <SchoolIcon className={className} />;
 };
