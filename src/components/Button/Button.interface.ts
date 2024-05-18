a260204d5961e2d08229e9a40d25ee729e06ecb7
AntonNikonov1 <125year125@gmail.com>
2024-05-18 11:22:06 +0200

    Create outline ui type for ButtonComponent.

--- src/components/Button/Button.interface.ts
+++ src/components/Button/Button.interface.ts
@@ -1,6 +1,6 @@
 import { ButtonProps } from "@mui/material";
 
 export interface ButtonComponentProps extends Omit<ButtonProps, "size"> {
-  ui?: "primary";
+  ui?: "primary" | "outline";
   size?: "xl";
 }
