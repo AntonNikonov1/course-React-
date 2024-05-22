4e1fc0a3d833547ac3c9fb5035f050e3c00c977b
AntonNikonov1 <125year125@gmail.com>
2024-05-22 13:34:08 +0200

    Connect MUI_THEME config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "@mui/material";
 import { AppRoutes } from "./pages/AppRoutes";
+import { MUI_THEME } from "./config/theme.config";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <ThemeProvider theme={MUI_THEME}>
+      <AppRoutes />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/config/theme.config.ts
@@ -0,0 +1,7 @@
+import { createTheme } from "@mui/material/styles";
+
+export const MUI_THEME = createTheme({
+  typography: {
+    fontFamily: `"Montserrat", sans-serif`,
+  },
+});
